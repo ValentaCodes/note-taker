ef542f86e860c6b5782756d5be0d93d539adc057
ValentaCodes <valentacodes@gmail.com>
2023-01-12 16:52:58 -0700

    changed port

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const notesRoute = require("./routes/notes.js");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 // Middleware
