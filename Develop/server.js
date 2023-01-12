cb41aa46f1a7f1650dfa1be29a2b6e11219c7631
ValentaCodes <valentacodes@gmail.com>
2023-01-12 16:07:14 -0700

    added process.env.PORT

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const path = require('path')
-const notesRoute = require('./routes/notes.js')
-const app = express();
-const PORT = 3000;
+const path = require("path");
+const notesRoute = require("./routes/notes.js");
 
+const PORT = process.env.PORT || 3000;
+
+const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // NOTE: Routes
-app.use('/api/notes', notesRoute)
+app.use("/api/notes", notesRoute);
 
 // retrieves the notes page
 app.get("/notes", (req, res) => {
@@ -21,7 +22,7 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public/index.html"))
 );
 // responds with 404 if page isn't available on server
-app.get('*', (req, res) => res.status(404).send('Page not found'))
+app.get("*", (req, res) => res.status(404).send("Page not found"));
 
 // Creates our server
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
