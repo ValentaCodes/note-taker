0086249de2b7c0bdb44e41f3d9e05ef1d46d20be
ValentaCodes <valentacodes@gmail.com>
2023-01-19 00:47:48 -0700

    Added comments JAN 19 2023

--- server.js
+++ server.js
@@ -1,28 +1,31 @@
-const express = require("express");
-const path = require("path");
-const notesRoute = require("./routes/notes.js");
-
+// Importing modules
+const express = require('express');
+const path = require('path');
+const notesRoute = require('./routes/notes.js');
+// creates an environment  variable for hosting service to use, if not use port 3001
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-// Middleware
+// Middleware used to help code function properly
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // NOTE: Routes
-app.use("/api/notes", notesRoute);
+// this will create a route so we can target and use our api
+app.use('/api/notes', notesRoute);
 
 // retrieves the notes page
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+
 });
 // retrieves landing page
-app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "public/index.html"))
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 // responds with 404 if page isn't available on server
-app.get("*", (req, res) => res.status(404).send("Page not found"));
+app.get('*', (req, res) => res.status(404).send('Page not found'));
 
 // Creates our server
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
